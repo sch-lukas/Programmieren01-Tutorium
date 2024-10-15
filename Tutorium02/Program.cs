c2f886c1fd3a6ea206989fd84eebf293c84aedc4
Lukas Schulz <lukasmschulz@gmail.com>
2024-10-15 17:11:40 +0200

    asas

--- Tutorium02/Program.cs
+++ Tutorium02/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using System;
+class Hauptklasse
+{
+    public static void Main(string[] args)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+        for (int j = 0; j < 6; j++)
+        {
+            Console.Write("*");
+        }
+        Console.WriteLine();
+        for (int j = 0; j < 6; j++)
+        {
+            Console.Write("*");
+        }
+        
+        
+        
+        
+    }
+}
