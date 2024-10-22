c5955c7c7b35a6c431f0bfe4b15f6b2fff6cf930
Lukas Schulz <lukasmschulz@gmail.com>
2024-10-22 17:14:52 +0200

    wqdfwq

--- Tutorium02/Program.cs
+++ Tutorium02/Program.cs
@@ -1,27 +1,37 @@
-﻿
+﻿using System;
 
-using System;
 class Hauptklasse
 {
     public static void Main(string[] args)
     {
-        for (int j = 0; j < 6; j++)
+        // Lese 5 Zahlen per Eingabe in der Konsole ein
+        // Gebe den Durchschnitt und die Größte Zahl aus
+
+        int max = 0; //oder int max = Int.MinValue, kleinste mögliche Int Zahl
+        int summe = 0; // Variabel, die für die Summe aller zahlen steht
+        for (int i = 0; i < 5; i++)
         {
-            Console.Write("*");
+            Console.Write("Bitte ganze Zahl eingeben: ");
+            int temp = Convert.ToInt32(Console.ReadLine()); //Zahl temporär speichern
+            summe = summe + temp; //Summe aufadierren und speichern
+            if (temp > max) //Überprüfe ob die neue Zahl größer ist als die alte und diese dann speichern
+            {max = temp; }
         }
-        Console.WriteLine();
-        for (int j = 0; j < 6; j++)
+        int j = 0;
+        while (j < 5)
         {
-            Console.Write("*");
+            j++;
+            Console.Write("Bitte ganze Zahl eingeben: ");
+            int temp = Convert.ToInt32(Console.ReadLine()); //Zahl temporär speichern
+
+            summe = summe + temp; //Summe aufadierren und speichern
+
+            if (temp > max) //Überprüfe ob die neue Zahl größer ist als die alte und diese dann speichern
+            {
+                max = temp;
+            }
         }
-        Console.WriteLine();
-        for (int j = 0; j < 6; j++)
-        {
-            Console.Write("*");
-        }
-        
-        
-        
-        
+        Console.WriteLine("Größte Zahl:" + max);
+        Console.WriteLine("Durchschnitt: " + summe / 5.0);
     }
 }
--- Tutroium03/Program.cs
+++ Tutroium03/Program.cs
@@ -1,17 +1,14 @@
-﻿
-
-
-class Haupt
+﻿class Haupt
 {
     public static void Main(string[] args)
     {
-        int counter = 0;
+        
         int leer = 9;
 
         for (int i = 0; i < 10; i++)
         {
-            Console.Write(counter);
-            counter++;
+            Console.Write(i);
+            
             for (int j = 0; j < leer; j++)
             {
                 Console.Write(" ");
@@ -19,5 +16,43 @@ class Haupt
             Console.WriteLine("*");
             leer--;
         }
+
+        int counter2 = 3; 
+        for (int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < counter2; j++){
+            for (int k = 0; k < 4; k++)
+            {
+                Console.Write("+");
+            }
+            Console.Write("-");
+            }
+
+            Console.WriteLine("");
+            counter2--;
+        }
+        
+        
+        
+        
     }
+
+    
+    
+    
+    
+    /*class Übung()
+    {
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) //10 Zahlen einspeichern
+        {
+            int speicher = Convert.ToInt32(Console.ReadLine());
+            int temp = speicher;
+            for (int j = 0; j < temp; j++) //Speicher mal 5 nehmen Speicher mal wiederholen
+            {
+                speicher = speicher * 5;
+            }
+            arr[i]= speicher;
+        }
+    }*/
 }
