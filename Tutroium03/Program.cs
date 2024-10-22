5e0e1faade947432b8c0118802881b28ad69a681
Lukas Schulz <lukasmschulz@gmail.com>
2024-10-22 15:36:52 +0200

    fwefe

---
+++ Tutroium03/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+class Haupt
+{
+    public static void Main(string[] args)
+    {
+        int counter = 0;
+        int leer = 9;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Console.Write(counter);
+            counter++;
+            for (int j = 0; j < leer; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("*");
+            leer--;
+        }
+    }
+}
