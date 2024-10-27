7ee1bb44a9b4edb5a097b3b3902e22fec8ee0232
lukas <lukasmschulz@gmail.com>
2024-10-27 11:39:30 +0100

    29.10

---
+++ Tutorium04/Program.cs
@@ -0,0 +1,35 @@
+﻿class Haupt
+{
+    public static void Main(string[] args)
+    {
+         /*
+         Aufgaben:
+         
+         1) 
+         Erstelle ein Array der Länge 10 für ganze Zahlen.
+         Belege das Array mithilfe einer Schleife mit Zahlen von 10 bis 1.
+         überschreibe die 5. Stelle des Arrays mit einer 70.
+         Gebe alle Werte des Arrays in der Konsole aus.
+         Gebe Länge des Arrays in der Konsole aus.
+         
+         
+         2)
+         Erstelle eine leere Liste für ganze Zahlen.
+         Füge mit einer Schleife Werte von 1 bis 10 hinzu.
+         Entferne den 7. Eintrag der Liste.
+         Füge eine 99 an die 3. Stelle hinzu.
+         Gebe die Liste in der Konsole aus.
+         Gebe die Größe der Liste aus.
+         
+         
+         3)
+         Berrechne den Durchschnitt der Liste und des Arrays und gebe ihn aus.
+         Gebe den maximalen Wert der Liste und des Arrays aus.
+        
+         */
+        
+        
+        
+        
+    }
+}
