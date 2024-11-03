8f577686896d00660ffcaee882ed5779a0b1dfaa
lukas <lukasmschulz@gmail.com>
2024-11-03 14:39:17 +0100

    1

---
+++ Tutorium05/Program.cs
@@ -0,0 +1,22 @@
+﻿class Haupt
+{
+    public static void Main(string[] args)
+    {
+        // Aufgabe01 Erstelle folgendes Array:
+        int[] test = new int[10];
+        Random zufall = new Random();
+        for (int i = 0; i < 10; i++)
+        {
+            test[i] = zufall.Next(10);
+        }
+        
+        // Erledige die folgenden Aufgabe in einer ausgelagerten void Methode/Funktion:
+        // Erstelle folgende Suchen und gebe das Ergebnis aus:
+        
+        // Wie oft kommt die Zahl 8 vor?
+        // Erstelle eine Liste die alle Indexe (Stelle im Array) der Zahl 5 enthält.
+        // Was ist die größte und kleinste Zahl im Array?
+        
+        // Welche Zahl kommt am meisten vor?
+    }
+}
