1d9aa0efa486b0efe547be629ec675ab2bc9d1e2
Lukas Schulz <lukasmschulz@gmail.com>
2024-11-07 14:09:19 +0100

    1

--- Tutorium05/Program.cs
+++ Tutorium05/Program.cs
@@ -9,14 +9,15 @@
         {
             test[i] = zufall.Next(10);
         }
-        
+
         // Erledige die folgenden Aufgabe in einer ausgelagerten void Methode/Funktion:
         // Erstelle folgende Suchen und gebe das Ergebnis aus:
         Console.Write("Das Array:");
         foreach (var v in test)
         {
-            Console.Write(v+", ");
+            Console.Write(v + ", ");
         }
+
         // Wie oft kommt die Zahl 8 vor?
         int counter = 0;
         foreach (int a in test)
@@ -26,61 +27,37 @@
                 counter++;
             }
         }
-        
-        Console.WriteLine("Wie oft kommt die 8 vor:"+counter);
-        
-        
-        
+
+        Console.WriteLine("Wie oft kommt die 8 vor:" + counter);
+
+
         // 5 _ _ 5 _ _ 5 _
         // 0 1 2 3 4 6 7 8
-        
+
         // List{0,3,7}
-        
+
         // Erstelle eine Liste die alle Indexe (Index=Stelle im Array) der Zahl 5 enthält.
-        
-        List <int> liste = new List<int>();
+
+        List<int> liste = new List<int>();
         for (int i = 0; i < test.Length; i++)
         {
-            if (test[i]==5)
+            if (test[i] == 5)
             {
                 liste.Add(i);
             }
         }
+
         Console.Write("Indexe von der Zahl fünf:");
         foreach (var x in liste)
         {
             Console.WriteLine(x);
         }
-        
-        
+
+
         // Erstelle eine Liste die alle Indexe (Index=Stelle im Array) der Zahl 5 enthält.
-        
+
         // Was ist die größte und kleinste Zahl im Array?
-        
+
         // Welche Zahl kommt am meisten vor?
     }
-    
-    
-    
-    
-    
-  
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
