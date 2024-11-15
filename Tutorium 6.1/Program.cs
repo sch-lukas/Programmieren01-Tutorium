3cfabe31b5837036b54387933708406e58cc11a7
Lukas Schulz <lukasmschulz@gmail.com>
2024-11-15 08:49:29 +0100

    1

---
+++ Tutorium 6.1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+struct Auto
+{
+     public string kennzeichen;
+     public int kilometerStand;
+     public Auto(int kilometerStandNeu, string kennzeichenNeu)
+     {
+         kennzeichen = kennzeichenNeu;
+         kilometerStand = kilometerStandNeu;
+     }
+     public void KilometerFahren(int kilometer)
+     {
+         kilometerStand = kilometerStand + kilometer;
+     }
+}
+class haupt
+{
+    public static void Main()
+    {
+        Auto a = new Auto();
+        a.kennzeichen = "Ka";
+        a.kilometerStand = 100;
+        Auto AMG_Mercedes = new Auto( 56000,"KA-BC");
+        AMG_Mercedes.KilometerFahren(10);
+        Console.WriteLine(AMG_Mercedes.kilometerStand);
+        Console.WriteLine(AMG_Mercedes.kennzeichen);
+    }
+}
--- Tutroium06/Program.cs
+++ Tutroium06/Program.cs
@@ -1,5 +1,4 @@
-﻿class Haupt
-{
+﻿
 
 
 /* Aufgabe 1: Durchschnitt, Minimum, Suchen
@@ -11,7 +10,7 @@ a.) Geben Sie eine Struktur Iphone an, die diese Daten speichern kann.
 b.) Erzeuge in der Main Methode eine Liste ,,iphones'' mit den folgenden Iphones:
     - 1. Iphone, lila, 150.5 Euro
     - 8. Iphone, rot, 348.55 Euro
-    - X. Iphone, blau, 654.95 Euro
+    - 10. Iphone, blau, 654.95 Euro
     - 11. Iphone, gelb, 674.25 Euro
     - 15. Iphone, rosa, 974.95 Euro
 
@@ -27,10 +26,70 @@ public static List<Iphone> NeuerAls (List<Iphone> iphones)
 
 }*/
 
+struct Iphone
+{
+    public string farbe;
+    public int generation;
+    public double preis;
 
+    public Iphone(string farbeNeu, int generationNeu, double preisNeu)
+    {
+        farbe = farbeNeu;
+        generation = generationNeu;
+        preis = preisNeu;
+    }
+
+    public static double DPreis(List<Iphone> iphones)
+    {
+        List<Iphone> iphonesTempär = new List<Iphone>();
+        foreach (var x in iphones)
+        {
+            if (x.preis < 660)
+            {
+                iphonesTempär.Add(x);
+            }
+        }
+        double durchschnitt = 0;
+        foreach (var y in iphonesTempär)
+        {
+            durchschnitt=durchschnitt + y.preis;
+        }
+        return durchschnitt/iphonesTempär.Count;
+    }
+
+    public static List<Iphone> NeuerAls(List<Iphone> iphones)
+    {
+        List<Iphone> neuerAls = new List<Iphone>();
+        int durchschnitt = 0;
+        foreach (var z in iphones)
+        {
+            durchschnitt = durchschnitt + z.generation;
+        }
+        durchschnitt = durchschnitt / iphones.Count;
+        foreach (var c in iphones)
+        {
+            if (c.generation > durchschnitt)
+            {
+                neuerAls.Add(c);
+            }
+        }
+        return neuerAls;
+    }
+    
+}
+class Hauptklasse
+{
     public static void Main(string[] args)
     {
+        Iphone lila = new Iphone("lila", 1, 150.5);
+        Iphone rot = new Iphone("rot", 8, 348.5);
+        Iphone blau = new Iphone("blau", 10, 654.95);
         
+        List<Iphone> IphoneLIste = new List<Iphone>();
+        IphoneLIste.Add(lila);
+        IphoneLIste.Add(rot);
+        IphoneLIste.Add(blau);
+        Iphone.DPreis(IphoneLIste);    
     }    
 
 }
