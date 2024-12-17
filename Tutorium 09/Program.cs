f6febeeb5c5e8fc44c246ed6932a6bf7c378f60d
Lukas Schulz <lukasmschulz@gmail.com>
2024-12-17 15:40:04 +0100

    1

--- Tutorium 09/Program.cs
+++ Tutorium 09/Program.cs
@@ -1,3 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+class AutoClass
+{
+    public int kilometerstand;
 
-Console.WriteLine("Hello, World!");
+    public int KiloSTAND
+    {
+        get { return kilometerstand; }
+        set
+        {
+            if (value < 0) throw new Exception("DUMM");
+            kilometerstand = value;
+        }
+    }
+    public override string ToString()
+    {
+        return string.Format("HALLO= {0} SERVUS {1} !",this.kilometerstand,this.kilometerstand);
+    }
+}
+
+struct AutoStruct
+{
+    public int kilometerstand ;
+}
+class haupt
+{
+    public static void Main(string[] args)
+    {
+        AutoStruct Auto1 = new AutoStruct();
+        AutoStruct Auto2 = new AutoStruct();
+
+        Auto1.kilometerstand = 1000;
+        Auto2.kilometerstand = Auto1.kilometerstand;
+        Auto1.kilometerstand = 2000;
+        Console.WriteLine("Auto1:"+Auto1.kilometerstand);
+        Console.WriteLine("Auto2:"+Auto2.kilometerstand);
+        
+        
+        AutoClass Auto3 = new AutoClass();
+        AutoClass Auto4 = new AutoClass();
+
+        Auto3.kilometerstand = 1000;
+        Auto4 = Auto3;
+        Auto3.kilometerstand = 2000;
+        Console.WriteLine(Auto3.KiloSTAND);
+        Auto3.KiloSTAND = 200;
+        Console.WriteLine(Auto3);
+
+    }    
+}
+
